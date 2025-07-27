6bc7165503676f92df3edde975406c9fc4042157
dima <vds788502@gmail.com>
2025-07-27 18:00:35 +0400

    Добавить файл init

---
+++ src/core/models/__init__.py
@@ -0,0 +1,12 @@
+__all__ = [
+    "Base",
+    "db_helper",
+    "VkTgChannel",
+    "TgChannel",
+    "AdvPosts"
+]
+
+from .base import Base
+from .db_helper import db_helper
+from .models import VkTgChannel, AdvPosts, TgChannel
+
